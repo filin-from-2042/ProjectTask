7b33adfb031778db07a3a8caca350d234fa2f357
Конов Алексей <AVKonov@sberbank.ru>
2024-08-08 13:44:16 +0300

    answer is ReadWriteLock

--- src/main/java/org/study/pixelbattleback/MapService.java
+++ src/main/java/org/study/pixelbattleback/MapService.java
@@ -13,6 +13,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Service
 public class MapService {
@@ -27,6 +29,8 @@ public class MapService {
     private final int[] colors;
 
     private boolean isChanged;
+
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     /**
      * Пытаемся загрузить карту из файла на старте, или же начинаем с пустой карты
@@ -54,14 +58,19 @@ public class MapService {
      * @param pixel
      * @return
      */
-    public synchronized boolean draw(PixelRequest pixel) {
+    public boolean draw(PixelRequest pixel) {
         int x = pixel.getX();
         int y = pixel.getY();
         if (x < 0 || x >= width || y < 0 || y >= height) {
             return false;
         }
-        colors[y * width + x] = pixel.getColor();
-        isChanged = true;
+        readWriteLock.writeLock().lock();
+        try {
+            colors[y * width + x] = pixel.getColor();
+            isChanged = true;
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
         return true;
     }
 
@@ -70,8 +79,13 @@ public class MapService {
      *
      * @return
      */
-    private synchronized int[] getColors() {
-        return Arrays.copyOf(colors, colors.length);
+    private int[] getColors() {
+        readWriteLock.readLock().lock();
+        try {
+            return Arrays.copyOf(colors, colors.length);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
     public Map getMap() {
@@ -86,17 +100,22 @@ public class MapService {
      * Периодически сохраняем карту в файл
      */
     @Scheduled(fixedDelay = 15, timeUnit = TimeUnit.SECONDS)
-    public synchronized void writeToFile() {
-        if (!isChanged) {
-            return;
-        }
-        isChanged = false;
-        try (FileOutputStream fileOutputStream = new FileOutputStream(MAP_BIN);
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
-            objectOutputStream.writeObject(getMap());
-            logger.info("Карта сохранена в файле {}", MAP_BIN);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+    public void writeToFile() {
+        readWriteLock.readLock().lock();
+        try {
+            if (!isChanged) {
+                return;
+            }
+            isChanged = false;
+            try (FileOutputStream fileOutputStream = new FileOutputStream(MAP_BIN);
+                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+                objectOutputStream.writeObject(getMap());
+                logger.info("Карта сохранена в файле {}", MAP_BIN);
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        } finally {
+            readWriteLock.readLock().unlock();
         }
     }
 
--- src/test/java/org/study/pixelbattleback/TestCase.java
+++ src/test/java/org/study/pixelbattleback/TestCase.java
@@ -61,14 +61,56 @@ public class TestCase {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println(String.format("%s     %s     %s     %s", stats.getCount(), stats.getMin(), stats.getAverage(), stats.getMax()));
+        System.out.println(stats);
     }
 
     @Test
-    public void testSequential() {
-        System.out.println("count     min     average     max");
-        for (int i = 0; i < 10; i++) {
-            testParallel();
+    public void testSequential() throws InterruptedException {
+        LongSummaryStatistics resStats = new LongSummaryStatistics();
+        for (int i = 0; i < 20; i++) {
+            resStats.combine(getStats());
+            Thread.sleep(1000);
         }
+        System.out.println(resStats);
+    }
+
+    public LongSummaryStatistics getStats() {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        List<Future<?>> futureList = new ArrayList<>();
+        for (int threadNumber = 0; threadNumber < THREAD_COUNT; ++threadNumber) {
+            int threadColor = ThreadLocalRandom.current().nextInt(1, 255<<17);
+            final RestTemplate rest = new RestTemplate();
+            futureList.add(pool.submit(() -> {
+                PixelRequest pixel = new PixelRequest();
+                pixel.setColor(threadColor);
+                StopWatch stopWatch = new StopWatch();
+                LongSummaryStatistics stats = new LongSummaryStatistics();
+                for (int i = 0; i < REQUEST_COUNT; i++) {
+                    pixel.setX(ThreadLocalRandom.current().nextInt(0, 100));
+                    pixel.setY(ThreadLocalRandom.current().nextInt(0, 100));
+                    stopWatch.start();
+                    try {
+                        ResultResponse res = rest.postForObject(URL, pixel, ResultResponse.class);
+                    }catch (Exception e){
+                        logger.error(e.getMessage(),e);
+                    }
+                    stopWatch.stop();
+                    stats.accept(stopWatch.getLastTaskTimeMillis());
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(1, 100));
+                }
+                return stats;
+            }));
+        }
+        LongSummaryStatistics stats = new LongSummaryStatistics();
+        for (Future<?> voidFuture : futureList) {
+            try {
+                stats.combine((LongSummaryStatistics) voidFuture.get());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return stats;
     }
 }
